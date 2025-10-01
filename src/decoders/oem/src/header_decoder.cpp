d2fa6e517c90d38cef2ed12d004be6d8d2e58a4c
Sean Ovens <sean.ovens@hexagon.com>
2025-10-01 16:53:10 -0600

    Fix clang format

--- src/decoders/oem/src/header_decoder.cpp
+++ src/decoders/oem/src/header_decoder.cpp
@@ -425,8 +425,7 @@ STATUS HeaderDecoder::Decode(const unsigned char* pucLogBuf_, IntermediateHeader
     stMetaData_.messageName =
         pclMyMsgDb->MsgIdToMsgName(CreateMsgId(stInterHeader_.usMessageId, 0, static_cast<uint32_t>(MESSAGE_FORMAT::ABBREV), 0U));
 
-    if (stInterHeader_.usMessageId > 0)
-        mapMyMessageIdCounts[stInterHeader_.usMessageId]++;
+    if (stInterHeader_.usMessageId > 0) { mapMyMessageIdCounts[stInterHeader_.usMessageId]++; }
 
     return STATUS::SUCCESS;
 }
